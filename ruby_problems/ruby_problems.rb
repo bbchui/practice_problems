fa6f031ac14375f52a9c0edd27461383e47dc9f9
Brandon Chui <bbchui325@gmail.com>
2017-08-02 13:17:41 -0700

    is perfect power

--- ruby_problems/ruby_problems.rb
+++ ruby_problems/ruby_problems.rb
@@ -1,7 +1,19 @@
 def isPP(n)
+  res = nil
   m = 2
   k = 2
-  until m**k >= n
-    k += 1
-
+  if Math.sqrt(n) % 1 == 0
+    return [Math.sqrt(n).to_i, 2]
+  end
+  until m >= Math.sqrt(n)
+    until m**k >= n
+      k += 1
+    end
+    if m**k == n
+      return [m, k]
+    end
+    m += 1
+    k = 2
+  end
+  res
 end
