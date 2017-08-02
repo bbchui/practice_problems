f6fe403fc16fc30b49c2cb35efb46e3e9fe3f972
Brandon Chui <bbchui325@gmail.com>
2017-08-02 14:30:45 -0700

    format duration

--- ruby_problems/ruby_problems.rb
+++ ruby_problems/ruby_problems.rb
@@ -17,3 +17,50 @@ def isPP(n)
   end
   res
 end
+
+def format_duration(seconds)
+  return "now" if seconds == 0
+  year = seconds / 31536000
+  day = (seconds % 31536000) / 86400
+  hour = ((seconds % 31536000) % 86400) / 3600
+  minute = (((seconds % 31536000) % 86400) % 3600) / 60
+  sec = ((((seconds % 31536000) % 86400) % 3600) % 60) % 60
+
+  y = plural("year", year)
+  d = plural("day", day)
+  h = plural("hour", hour)
+  m = plural("minute", minute)
+  s = plural("second", sec)
+
+  res = []
+  res.push("#{year} #{y}") if y
+  res.push("#{day} #{d}") if d
+  res.push("#{hour} #{h}") if h
+  res.push("#{minute} #{m}") if m
+  res.push("#{sec} #{s}") if s
+
+  if res.length > 2
+    i = 0
+    until i >= res.length - 2
+      res[i] = res[i] + ","
+      i += 1
+    end
+    res.insert(res.length - 1, "and").join(" ")
+  elsif res.length > 1
+    res.insert(res.length - 1, "and").join(" ")
+  else
+    res.join(" ")
+  end
+end
+
+def plural(time, num)
+  if num == 0
+    return nil
+  elsif num == 1
+    return "#{time}"
+  else
+    return "#{time}s"
+  end
+end
+
+p format_duration(3662)
