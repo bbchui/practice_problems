8ce7af67410becd6434469ed87611717bc5426ea
Brandon Chui <bbchui325@gmail.com>
2018-02-15 00:18:03 -0800

    hourglass problem

--- javascript_problems/practice.js
+++ javascript_problems/practice.js
@@ -30,26 +30,56 @@
 //
 // console.log(bottom_up_fib(35));
 
-function coinChange(amount, coins){
-    // Complete this function
-    let table = [];
-    for (let i = 0; i < amount + 1; i++) {
-        table[i] = 0
-    }
-    table[0] = 1
+// function coinChange(amount, coins){
+//     // Complete this function
+//     let table = [];
+//     for (let i = 0; i < amount + 1; i++) {
+//         table[i] = 0
+//     }
+//     table[0] = 1
+//
+//     for (let j = 0; j < coins.length; j++) {
+//         let coin = coins[j];
+//
+//         for (let k = coin; k <= amount; k++) {
+//             let remainder = k - coin
+//             // creates table of [1,1,1,1,1,1....]
+//             table[k] += table[remainder]
+//             // next coin will add 1
+//         }
+//         console.log(table);
+//     }
+//     console.log(table[amount])
+// }
+//
+// coinChange(6, [1,5])
 
-    for (let j = 0; j < coins.length; j++) {
-        let coin = coins[j];
-
-        for (let k = coin; k <= amount; k++) {
-            let remainder = k - coin
-            // creates table of [1,1,1,1,1,1....]
-            table[k] += table[remainder]
-            // next coin will add 1
-        }
-        console.log(table);
+function hourglass(arr) {
+  let max = -63
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 4; j++) {
+      let sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+      console.log(sum);
+      max = sum > max ? sum : max
     }
-    console.log(table[amount])
+  }
+  console.log(max);
 }
 
-coinChange(6, [1,5])
+let arr1 = [[1,1,1,0,0,0],
+           [0,1,0,0,0,0],
+           [1,1,1,0,0,0],
+           [0,9,2,-4,-4,0],
+           [0,0,0,-2,0,0],
+           [0,0,-1,-2,-4,0]]
+
+// hourglass(arr1)
+
+let arr2 = [[-1,1,-1,0,0,0],
+            [0,-1,0,0,0,0],
+            [-1,-1,-1,0,0,0],
+            [0,-9,2,-4,-4,0],
+            [-7,0,0,-2,0,0],
+            [0,0,-1,-2,-4,0]]
+
+hourglass(arr2)
