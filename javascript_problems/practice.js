3e116b80c14820ab16b583280932e18f7b24d9ca
Brandon Chui <bbchui325@gmail.com>
2018-03-21 03:08:49 -0700

    threeSum to 0

--- javascript_problems/practice.js
+++ javascript_problems/practice.js
@@ -232,29 +232,58 @@
 //
 // console.log(needle("str","lajnstkjfstrkjnerostruaiuz"));
 
-var letterCombinations = function(digits, current = "", res = []) {
-    let dict = {2 : ['a','b','c'],
-                3 : ['d','e','f'],
-                4 : ['g','h','i'],
-                5 : ['j','k','l'],
-                6 : ['m','n','o'],
-                7 : ['p','q','r','s'],
-                8 : ['t','u','v'],
-                9 : ['w','x','y','z']}
+// var letterCombinations = function(digits, current = "", res = []) {
+//     let dict = {2 : ['a','b','c'],
+//                 3 : ['d','e','f'],
+//                 4 : ['g','h','i'],
+//                 5 : ['j','k','l'],
+//                 6 : ['m','n','o'],
+//                 7 : ['p','q','r','s'],
+//                 8 : ['t','u','v'],
+//                 9 : ['w','x','y','z']}
+//
+//     if(digits.length === 0 && current === "") {
+//         return res
+//     }
+//     if (digits.length < 1) {
+//         return res.push(current)
+//     }
+//
+//     let currentNumber = dict[digits[0]]
+//
+//     for (let i = 0; i < currentNumber.length; i++) {
+//         currentLetter = dict[digits[0]][i]
+//         letterCombinations(digits.slice(1), current + currentLetter, res)
+//     }
+//
+//     return res
+// };
 
-    if(digits.length === 0 && current === "") {
-        return res
+var threeSum = function(nums) {
+    nums = nums.sort((a,b) => a - b)
+    let res = []
+    for (let i = 0; i < nums.length; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) {
+          continue
+        }
+        for (let j = i + 1, k = nums.length - 1; j < k;) {
+            if (nums[i] + nums[j] + nums[k] === 0) {
+                res.push([nums[i], nums[j], nums[k]])
+                j++
+                k--
+                while (j < k && nums[j] === nums[j - 1]) {
+                    j++
+                }
+                while (j < k && nums[k] === nums[k + 1]) {
+                    k--
+                }
+            } else if (nums[i] + nums[j] + nums[k] > 0) {
+                k--
+            } else if (nums[i] + nums[j] + nums[k] < 0) {
+                j++
+            }
+        }
+
     }
-    if (digits.length < 1) {
-        return res.push(current)
-    }
-
-    let currentNumber = dict[digits[0]]
-
-    for (let i = 0; i < currentNumber.length; i++) {
-        currentLetter = dict[digits[0]][i]
-        letterCombinations(digits.slice(1), current + currentLetter, res)
-    }
-
     return res
 };
