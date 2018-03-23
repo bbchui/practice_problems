7398ca2412a2e2c1e5cc8226eedb81db89067849
Brandon Chui <bbchui325@gmail.com>
2018-03-23 15:42:34 -0700

    fibonacci memoization

--- javascript_problems/practice.js
+++ javascript_problems/practice.js
@@ -259,31 +259,54 @@
 //     return res
 // };
 
-var threeSum = function(nums) {
-    nums = nums.sort((a,b) => a - b)
-    let res = []
-    for (let i = 0; i < nums.length; i++) {
-        if (i > 0 && nums[i] === nums[i - 1]) {
-          continue
-        }
-        for (let j = i + 1, k = nums.length - 1; j < k;) {
-            if (nums[i] + nums[j] + nums[k] === 0) {
-                res.push([nums[i], nums[j], nums[k]])
-                j++
-                k--
-                while (j < k && nums[j] === nums[j - 1]) {
-                    j++
-                }
-                while (j < k && nums[k] === nums[k + 1]) {
-                    k--
-                }
-            } else if (nums[i] + nums[j] + nums[k] > 0) {
-                k--
-            } else if (nums[i] + nums[j] + nums[k] < 0) {
-                j++
-            }
-        }
+// var threeSum = function(nums) {
+//     nums = nums.sort((a,b) => a - b)
+//     let res = []
+//     for (let i = 0; i < nums.length; i++) {
+//         if (i > 0 && nums[i] === nums[i - 1]) {
+//           continue
+//         }
+//         for (let j = i + 1, k = nums.length - 1; j < k;) {
+//             if (nums[i] + nums[j] + nums[k] === 0) {
+//                 res.push([nums[i], nums[j], nums[k]])
+//                 j++
+//                 k--
+//                 while (j < k && nums[j] === nums[j - 1]) {
+//                     j++
+//                 }
+//                 while (j < k && nums[k] === nums[k + 1]) {
+//                     k--
+//                 }
+//             } else if (nums[i] + nums[j] + nums[k] > 0) {
+//                 k--
+//             } else if (nums[i] + nums[j] + nums[k] < 0) {
+//                 j++
+//             }
+//         }
+//
+//     }
+//     return res
+// };
 
-    }
-    return res
-};
+
+//fibonacci memoization
+let memoization_table = {}
+
+for(let i = 0; i < 50; i++) {
+  memoization_table[i] = null
+}
+
+const fib = (n) => {
+  if (n === 0) {
+    return 0
+  } else if (n === 1 || n === 2) {
+    return 1
+  } else if (memoization_table[n]) {
+    return memoization_table[n]
+  }
+
+  memoization_table[n] = fib(n - 2) + fib(n - 1)
+  return memoization_table[n]
+}
+
+console.log(fib(40));
